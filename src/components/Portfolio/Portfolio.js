378055547b837a951093256343ad4d058adc6da9
Cody Cooper <codycooper06@gmail.com>
2022-10-26 16:07:03 -0400

    added project descriptions

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -8,37 +8,43 @@ const ProjectList = () => {
       name: 'Run Buddy',
       link: 'https://konopie.github.io/run-buddy/',
       src: 'run-buddy',
-      github: 'https://github.com/Konopie/run-buddy'
+      github: 'https://github.com/Konopie/run-buddy',
+      description: 'Main page for an exercise app that has multiple forms and flexbox for a reactive layout'
     },
     {
       name: 'Cat Generator',
       link: 'https://konopie.github.io/cat-generator/',
       src: 'cat-genie',
-      github: 'https://github.com/Konopie/cat-generator'
+      github: 'https://github.com/Konopie/cat-generator',
+      description: 'Cat site that uses APIs to generate cat pictures and cat facts on request'
     },
     {
       name: 'Hungry Guys',
       link: 'https://hungry-guys.herokuapp.com/',
       src: 'hungry-guys',
-      github: 'https://github.com/Konopie/hungry-guys'
+      github: 'https://github.com/Konopie/hungry-guys',
+      description: 'Recipie sharing platform with login, posts, and comments'
     },
     {
       name: 'Just Another Text Editor',
       link: 'https://konopies-text-editor.herokuapp.com/',
       src: 'JATE',
-      github: 'https://github.com/Konopie/text-editor'
+      github: 'https://github.com/Konopie/text-editor',
+      description: 'PWA Text editor that uses IndexDB for data persistence that also utilizes service workers to function offline'
     },
     {
       name: 'Note Taker',
       link: 'https://tranquil-shore-49299.herokuapp.com/',
       src: 'note-taker',
-      github: 'https://github.com/Konopie/note-taker'
+      github: 'https://github.com/Konopie/note-taker',
+      description: 'Node taker that uses local storage to store and retrieve note data'
     },
     {
       name: 'Football Fantasy',
       link: 'https://fantasy-football-draft-app.herokuapp.com/',
       src: 'football-fantasy',
-      github: 'https://github.com/MosNes/fantasy-football-draft'
+      github: 'https://github.com/MosNes/fantasy-football-draft',
+      description: 'Fantasy Football site that allows to create an account, a team, a league, and simultaneously start a global draft with league members on different devices'
     },
   ];
 
@@ -52,6 +58,7 @@ const ProjectList = () => {
           <div className={`img ${project.src}`}>
           <a className='portfolio-link' href = {`${project.link}`}>Website</a>
           <a className='portfolio-link' href = {`${project.github}`}>GitHub</a>
+          <p className='portfolio-desc'>{`${project.description}`}</p>
           {/* <img
             src={require(`../../assets/${project.src}`)}
             alt={project.name}
